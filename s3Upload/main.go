e52955f4a813971ed96dcb8c0485a7c435f40ab1
Sinan Bolel <dev@sinanbolel.com>
2014-09-11 08:44:19 -0500

    Fixed URL error in s3upload.

--- s3Upload/main.go
+++ s3Upload/main.go
@@ -10,11 +10,9 @@ func UploadImg(f io.Reader, l string) (int, string) {
   log.Println("UploadImage run with l:", l)
   s3util.DefaultConfig.AccessKey = os.Getenv("ECHO_DEV_S3_ACCESS_KEY")
   s3util.DefaultConfig.SecretKey = os.Getenv("ECHO_DEV_S3_SECRET_KEY")
-  s3Url := os.Getenv("ECHO_DEV_S3_URL")
-  //Upload Location
-  ul := s3Url +"/"+ l
+  // s3Url := os.Getenv("ECHO_DEV_S3_URL")
   //Create new s3 object
-  s3w, _ := s3util.Create(ul, nil, nil)
+  s3w, _ := s3util.Create(l, nil, nil)
   //Copy file to s3 object
   p, err := io.Copy(s3w, f)
   //Log Upload Size
@@ -26,5 +24,5 @@ func UploadImg(f io.Reader, l string) (int, string) {
   }
   s3w.Close()
   log.Println("UploadImage successful")
-  return 200, ul
+  return 200, l
 }
